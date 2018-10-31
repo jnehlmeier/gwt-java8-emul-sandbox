gwtproject/gwt

Add JEP286 (Local-Variable Type Inference) support.
DO NOT MERGE: This patch requires updated ASM and JDT.
These are sended in PR https://github.com/gwtproject/tools/pull/14.

Until PR merged, ASM and JDT updates could be picked from
git@github.com:axls/tools.git. E.g:

git pull git@github.com:axls/tools.git java10

into tools folder.

Bug: #9609
Bug-Link: https://github.com/gwtproject/gwt/issues/9609
Change-Id: Ia2d7e8af11d78b31ac0a4db0b2a0205324b6de0e

dev/core/src/com/google/gwt/dev/jjs/impl/ReferenceMapper.java
@@ -288,7 +288,7 @@ public class ReferenceMapper {
   }
 
   private JDeclaredType createType(ReferenceBinding binding) {
-    String name = JdtUtil.asDottedString(binding.compoundName);
+    String name = JdtUtil.getClassName(binding);
     SourceInfo info = SourceOrigin.UNKNOWN;
     if (binding.isClass()) {
       return new JClassType(info, name, binding.isAbstract(), binding.isFinal());

jnehlmeier/gwt-java8-emul-sandbox

dev/core/src/com/google/gwt/dev/jjs/impl/ReferenceMapper.java
@@ -288,7 +288,7 @@ public class ReferenceMapper {
   }
 
   private JDeclaredType createType(ReferenceBinding binding) {
-    String name = JdtUtil.asDottedString(binding.compoundName);
+    String name = JdtUtil.getQualifiedSourceName(binding);
     SourceInfo info = SourceOrigin.UNKNOWN;
     if (binding.isClass()) {
       return new JClassType(info, name, binding.isAbstract(), binding.isFinal());